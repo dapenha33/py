598d7df9bcc2b08e53362c96c2ceb6f27ee2b9d9
dapenha33 <josuelabomfim@gmail.com>
2026-02-11 16:51:43 -0300

    Add files via upload

---
+++ operadores.py
@@ -0,0 +1,11 @@
+import os
+
+os.system("cls")
+
+numero = int (input('digite um numero'))
+
+sucessor = int (input (numero  + 1))
+antecessor = int (numero - 1)
+
+print ('sucessor', sucessor)
+print ('antecessor', antecessor)
